d9917cdc87b718959c3535eb11cc8a73ace40af3
dougal <dougal@e4d4d81e-240f-4a72-d798-d15127759858>
2007-09-18 16:14:00 +0000

    Fixed some access-control issues. Added details rdoc documentation comments.
    git-svn-id: http://svn.douglasfshearer.com/rails/plugins/acts_as_indexed@27 e4d4d81e-240f-4a72-d798-d15127759858

--- lib/acts_as_indexed.rb
+++ lib/acts_as_indexed.rb
@@ -5,18 +5,30 @@
 
 require 'active_record'
 
-module Foo
+require 'search_index'
+require 'search_atom'
+
+module Foo #:nodoc:
   module Acts #:nodoc:
     module Indexed #:nodoc:
-      
-      require 'search_index'
-      require 'search_atom'
       
       def self.included(mod)
         mod.extend(ClassMethods)
       end
 
       module ClassMethods
+
+        # Declares a class as searchable.
+        #
+        # ====options:
+        # fields:: Names of fields to include in the index. Symbols pointing to
+        #          instance methods of your model may also be given here.
+        # index_file_depth:: Tuning value for the index partitioning. Larger
+        #                    values result in quicker searches, but slower
+        #                    indexing. Default is 3.
+        # min_word_size:: Sets the minimum length for a word in a query. Words
+        #                 shorter than this value are ignored in searches
+        #                 unless preceded by the '+' operator. Default is 3.
 
         def acts_as_indexed(options = {})
           class_eval do
@@ -54,29 +66,47 @@ module Foo
 
         end
 
+        # Adds the passed +record+ to the index. Index is built if it does not already exist. Clears the query cache.
+        
         def index_add(record)
           index = SearchIndex.new(aai_config[:index_file], aai_config[:index_file_depth], aai_config[:fields], aai_config[:min_word_size])
-          build_index if !index.root_exists?
+          build_index if !index.exists?
           index.add_record(record)
           index.save
           @results_cache = {}
           true
         end
-
+        
+        # Removes the passed +record+ from the index. Clears the query cache.
+        
         def index_remove(record)
           index = SearchIndex.new(aai_config[:index_file], aai_config[:index_file_depth], aai_config[:fields], aai_config[:min_word_size])
-          build_index if !index.root_exists?
+          # record won't be in index if it doesn't exist. Just return true.
+          return true if !index.exists?
           index.remove_record(record)
           index.save
           @results_cache = {}
           true
         end
-
+        
+        # Finds instances matching the terms passed in +query+. Terms are ANDed by
+        # default. Returns an array of model instances or, if +ids_only+ is
+        # true, an array of integer IDs.
+        #
+        # Keeps a cache of matched IDs for the current session to speed up
+        # multiple identical searches.
+        #
+        # ====find_options
+        # A hash passed on to active_record's find when retrieving the data from db, useful for pagination.
+        #
+        # ====options
+        # ids_only:: Method returns an array of integer IDs when set to true.
+        
         def search_index(query, find_options={}, options={})
           if !@results_cache || !@results_cache[query]
             logger.debug('Query not in cache, running search.')
             index = SearchIndex.new(aai_config[:index_file], aai_config[:index_file_depth], aai_config[:fields], aai_config[:min_word_size])
-            build_index if !index.root_exists?
+            build_index if !index.exists?
             index.save
             @results_cache = {} if !@results_cache
             @results_cache[query] = index.search(query)
@@ -92,7 +122,8 @@ module Foo
         end
 
         private
-
+        
+        # Builds an index from scratch for the current model class.
         def build_index
           index = SearchIndex.new(aai_config[:index_file], aai_config[:index_file_depth], aai_config[:fields], aai_config[:min_word_size])
           index.add_records(find(:all))
@@ -101,25 +132,37 @@ module Foo
 
       end
 
-      # Adds singleton methods.
+      # Adds model class singleton methods.
       module SingletonMethods
-
+        
+        # Finds instances matching the terms passed in +query+.
+        #
+        # See Foo::Acts::Indexed::ClassMethods#search_index.
         def find_with_index(query='', find_options = {}, options = {})
           search_index(query, find_options, options)
         end
 
       end
 
-      # Adds instance methods.
+      # Adds model class instance methods.
+      # Methods are called automatically by ActiveRecord on +save+, +destroy+,
+      # and +update+ of model instances.
       module InstanceMethods
+        
+        # Adds the current model instance to index.
+        # Called by ActiveRecord on +save+.
         def add_to_index
           self.class.index_add(self)
         end
 
+        # Removes the current model instance to index.
+        # Called by ActiveRecord on +destroy+.
         def remove_from_index
           self.class.index_remove(self)
         end
 
+        # Updates current model instance index.
+        # Called by ActiveRecord on +update+.
         def update_index
           self.class.index_remove(self)
           self.class.index_add(self)
--- lib/search_atom.rb
+++ lib/search_atom.rb
@@ -3,44 +3,57 @@
 # http://douglasfshearer.com
 # Distributed under the MIT license as included with this plugin.
 
-class SearchAtom
-  
-  # Contains a hash of records.
-  # { 'record_id' => [pos1, pos2, pos] }
+module Foo #:nodoc:
+  module Acts #:nodoc:
+    module Indexed #:nodoc:
+      class SearchAtom
 
-  def initialize
-    @records = {}
-  end
-  
-  # Returns true if the given record is present.
-  def include_record?(record_id)
-    @records.include?(record_id)
-  end
-  
-  def add_record(record_id)
-    @records[record_id] = [] if !include_record?(record_id)
-  end
+        # Contains a hash of records.
+        # { 'record_id' => [pos1, pos2, pos] }
 
-  def add_position(record_id, pos)
-    add_record(record_id)
-    @records[record_id] << pos
-  end
-  
-  def record_ids
-    @records.keys
-  end
-  
-  def positions(record_id)
-    return @records[records] if include_record?(record_id)
-    nil
-  end
-  
-  def to_h
-    @records
-  end
-  
-  def remove_record(record_id)
-    @records.delete(record_id)
-  end
+        def initialize
+          @records = {}
+        end
+        
+        # Returns true if the given record is present.
+        def include_record?(record_id)
+          @records.include?(record_id)
+        end
 
+        # Adds +record_id+ to the stored records.
+        def add_record(record_id)
+          @records[record_id] = [] if !include_record?(record_id)
+        end
+
+        # Adds +pos+ to the array of positions for +record_id+.
+        def add_position(record_id, pos)
+          add_record(record_id)
+          @records[record_id] << pos
+        end
+        
+        # Returns all record IDs stored in this Atom.
+        def record_ids
+          @records.keys
+        end
+
+        # Returns an array of positions for +record_id+ stored in this Atom.
+        def positions(record_id)
+          return @records[records] if include_record?(record_id)
+          nil
+        end
+
+        # Converts Atom to a hash of form:
+        #  { record_id => [ pos1, po2, pos3 ] }
+        def to_h
+          @records
+        end
+        
+        # Removes +record_id+ from this Atom.
+        def remove_record(record_id)
+          @records.delete(record_id)
+        end
+
+      end
+    end
+  end
 end
