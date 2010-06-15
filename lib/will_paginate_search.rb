3dfdb810f11d103d188022652b565bf1f9b75b92
Alex Coles <alex@alexcolesportfolio.com>
2010-06-15 12:34:56 +0200

    Rails 3: Patch acts_as_indexed for WillPaginate 3
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- lib/will_paginate_search.rb
+++ lib/will_paginate_search.rb
@@ -2,11 +2,11 @@
 # Copyright (c) 2007 - 2010 Douglas F Shearer.
 # http://douglasfshearer.com
 
-module WillPaginate
+module Foo::Acts::Indexed
 
-  module Finder
+   module WillPaginate
 
-    module ClassMethods
+     module Search
 
       # DEPRECATED. Use chained pagination instead.
       def paginate_search(query, options)
@@ -15,7 +15,7 @@ module WillPaginate
 
         total_entries ||= find_with_index(query,{},{:ids_only => true}).size
 
-        returning WillPaginate::Collection.new(page, per_page, total_entries) do |pager|
+        returning ::WillPaginate::Collection.new(page, per_page, total_entries) do |pager|
           options.update :offset => pager.offset, :limit => pager.per_page
 
           options = options.delete_if {|key, value| [:page, :per_page].include?(key) }
@@ -26,4 +26,8 @@ module WillPaginate
 
     end
   end
+end
+
+class ActiveRecord::Base
+  extend Foo::Acts::Indexed::WillPaginate::Search
 end
