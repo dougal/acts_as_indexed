2ca8f3fb1b9d921fdd51af83eff4799bcba6bb0a
Douglas F Shearer <dougal.s@gmail.com>
2010-09-06 20:55:26 +0100

    Fixed some of the deprecation errors while running tests. Added some nice helper methods to tidy up tests.

--- test/acts_as_indexed_test.rb
+++ test/acts_as_indexed_test.rb
@@ -14,32 +14,26 @@ class ActsAsIndexedTest < ActiveSupport::TestCase
     assert_equal [], Post.find_with_index('badger')
     post = Post.new(:title => 'badger', :body => 'Thousands of them!')
     assert post.save
-    assert_equal original_post_count+1, Post.count
-    assert_equal [post.id], Post.find_with_index('badger',{},{:ids_only => true})
+    assert_equal original_post_count + 1, Post.count
+    assert_equal [post.id], result_ids('badger')
   end
 
   def test_removes_from_index
     original_post_count = Post.count
-    assert_equal [posts(:wikipedia_article_4).id], Post.find_with_index('album',{},{:ids_only => true})
+    assert_equal [posts(:wikipedia_article_4).id], result_ids('album')
     assert Post.find(posts(:wikipedia_article_4).id).destroy
-    assert_equal [], Post.find_with_index('album',{},{:ids_only => true})
-    assert_equal original_post_count-1, Post.count
+    assert_equal [], result_ids('album')
+    assert_equal original_post_count - 1, Post.count
   end
 
   def test_search_returns_posts
-    Post.find_with_index('album').each do |p|
-      assert_equal Post, p.class
-    end
-  end
-
-  def test_scoped_search_returns_posts
     Post.with_query('album').each do |p|
       assert_equal Post, p.class
     end
   end
 
   def test_search_returns_post_ids
-    Post.find_with_index('album',{},{:ids_only => true}).each do |pid|
+    result_ids('album').each do |pid|
       assert p = Post.find(pid)
       assert_equal Post, p.class
     end
@@ -49,95 +43,72 @@ class ActsAsIndexedTest < ActiveSupport::TestCase
   # the portion removes should no longer be in the index.
   def test_updates_index
     p = Post.create(:title => 'A special title', :body => 'foo bar bla bla bla')
-    assert Post.find_with_index('title',{},{:ids_only => true}).include?(p.id)
+    assert result_ids('title').include?(p.id)
     p.update_attributes(:title => 'No longer special')
-    assert !Post.find_with_index('title',{},{:ids_only => true}).include?(p.id)
+    assert !result_ids('title').include?(p.id)
   end
 
   def test_simple_queries
-    assert_equal [],  Post.find_with_index(nil)
-    assert_equal [],  Post.find_with_index('')
-    assert_equal [5, 6],  Post.find_with_index('ship',{},{:ids_only => true}).sort
-    assert_equal [6],  Post.find_with_index('foo',{},{:ids_only => true})
-    assert_equal [6],  Post.find_with_index('foo ship',{},{:ids_only => true})
-    assert_equal [6],  Post.find_with_index('ship foo',{},{:ids_only => true})
-  end
-
-  def test_scoped_simple_queries
-    assert_equal [],  Post.find_with_index(nil)
-    assert_equal [],  Post.with_query('')
-    assert_equal [5, 6],  Post.with_query('ship').map{|r| r.id}.sort
-    assert_equal [6],  Post.with_query('foo').map{|r| r.id}
-    assert_equal [6],  Post.with_query('foo ship').map{|r| r.id}
-    assert_equal [6],  Post.with_query('ship foo').map{|r| r.id}
+    assert_equal_array_contents [],     result_ids(nil)
+    assert_equal_array_contents [],     result_ids('')
+    assert_equal_array_contents [5, 6], result_ids('ship')
+    assert_equal_array_contents [6],    result_ids('foo')
+    assert_equal_array_contents [6],    result_ids('foo ship')
+    assert_equal_array_contents [6],    result_ids('ship foo')
   end
 
   def test_negative_queries
-    assert_equal [5, 6],  Post.find_with_index('crane',{},{:ids_only => true}).sort
-    assert_equal [5],  Post.find_with_index('crane -foo',{},{:ids_only => true})
-    assert_equal [5],  Post.find_with_index('-foo crane',{},{:ids_only => true})
-    assert_equal [],  Post.find_with_index('-foo') #Edgecase
-  end
-
-  def test_scoped_negative_queries
-    assert_equal [5, 6],  Post.with_query('crane').map{|r| r.id}.sort
-    assert_equal [5],  Post.with_query('crane -foo').map{|r| r.id}
-    assert_equal [5],  Post.with_query('-foo crane').map{|r| r.id}
-    assert_equal [],  Post.with_query('-foo') #Edgecase
+    assert_equal_array_contents [5, 6], result_ids('crane')
+    assert_equal_array_contents [5],    result_ids('crane -foo')
+    assert_equal_array_contents [5],    result_ids('-foo crane')
+    assert_equal_array_contents [],     result_ids('-foo') #Edgecase
   end
 
   def test_quoted_queries
-    assert_equal [5],  Post.find_with_index('"crane ship"',{},{:ids_only => true})
-    assert_equal [6],  Post.find_with_index('"crane big"',{},{:ids_only => true})
-    assert_equal [],  Post.find_with_index('foo "crane ship"')
-    assert_equal [],  Post.find_with_index('"crane badger"')
-  end
-
-  def test_scoped_quoted_queries
-    assert_equal [5],  Post.with_query('"crane ship"').map{|r| r.id}
-    assert_equal [6],  Post.with_query('"crane big"').map{|r| r.id}
-    assert_equal [],  Post.with_query('foo "crane ship"')
-    assert_equal [],  Post.with_query('"crane badger"')
+    assert_equal_array_contents [5], result_ids('"crane ship"')
+    assert_equal_array_contents [6], result_ids('"crane big"')
+    assert_equal_array_contents [],  result_ids('foo "crane ship"')
+    assert_equal_array_contents [],  result_ids('"crane badger"')
   end
 
   def test_negative_quoted_queries
-    assert_equal [6],  Post.find_with_index('crane -"crane ship"',{},{:ids_only => true})
-    assert_equal [],  Post.find_with_index('-"crane big"',{},{:ids_only => true}) # Edgecase
+    assert_equal_array_contents [6], result_ids('crane -"crane ship"')
+    assert_equal_array_contents [],  result_ids('-"crane big"') # Edgecase
   end
 
   def test_scoped_negative_quoted_queries
-    assert_equal [6],  Post.with_query('crane -"crane ship"').map{|r| r.id}
-    assert_equal [],  Post.with_query('-"crane big"') # Edgecase
+    assert_equal_array_contents [6], result_ids('crane -"crane ship"')
+    assert_equal_array_contents [],  result_ids('-"crane big"') # Edgecase
   end
 
   def test_start_queries
-    assert_equal [6,5],  Post.find_with_index('ship ^crane',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^crane ship',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^ship ^crane',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^crane ^ship',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^ship crane',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('crane ^ship',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^crane',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^cran',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^cra',{},{:ids_only => true})
-    assert_equal [6,5,4],  Post.find_with_index('^cr',{},{:ids_only => true})
-    assert_equal [6,5,4,3,2,1], Post.find_with_index('^c',{},{:ids_only => true})
-    assert_equal [], Post.find_with_index('^notthere',{},{:ids_only => true})
+    assert_equal_array_contents [6,5],         result_ids('ship ^crane')
+    assert_equal_array_contents [6,5],         result_ids('^crane ship')
+    assert_equal_array_contents [6,5],         result_ids('^ship ^crane')
+    assert_equal_array_contents [6,5],         result_ids('^crane ^ship')
+    assert_equal_array_contents [6,5],         result_ids('^ship crane')
+    assert_equal_array_contents [6,5],         result_ids('crane ^ship')
+    assert_equal_array_contents [6,5],         result_ids('^crane')
+    assert_equal_array_contents [6,5],         result_ids('^cran')
+    assert_equal_array_contents [6,5],         result_ids('^cra')
+    assert_equal_array_contents [6,5,4],       result_ids('^cr')
+    assert_equal_array_contents [6,5,4,3,2,1], result_ids('^c')
+    assert_equal_array_contents [],            result_ids('^notthere')
   end
 
   def test_start_quoted_queries
-    assert_equal [6,5],  Post.find_with_index('^"crane" ship',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('ship ^"crane"',{},{:ids_only => true})
-    assert_equal [5],  Post.find_with_index('^"crane ship"',{},{:ids_only => true})
-    assert_equal [5],  Post.find_with_index('^"crane shi"',{},{:ids_only => true})
-    assert_equal [5],  Post.find_with_index('^"crane sh"',{},{:ids_only => true})
-    assert_equal [5],  Post.find_with_index('^"crane s"',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^"crane "',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^"crane"',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^"cran"',{},{:ids_only => true})
-    assert_equal [6,5],  Post.find_with_index('^"cra"',{},{:ids_only => true})
-    assert_equal [6,5,4],  Post.find_with_index('^"cr"',{},{:ids_only => true})
-    assert_equal [6,5,4,3,2,1], Post.find_with_index('^"c"',{},{:ids_only => true})
+    assert_equal_array_contents [6,5],         result_ids('^"crane" ship')
+    assert_equal_array_contents [6,5],         result_ids('ship ^"crane"')
+    assert_equal_array_contents [5],           result_ids('^"crane ship"')
+    assert_equal_array_contents [5],           result_ids('^"crane shi"')
+    assert_equal_array_contents [5],           result_ids('^"crane sh"')
+    assert_equal_array_contents [5],           result_ids('^"crane s"')
+    assert_equal_array_contents [6,5],         result_ids('^"crane "')
+    assert_equal_array_contents [6,5],         result_ids('^"crane"')
+    assert_equal_array_contents [6,5],         result_ids('^"cran"')
+    assert_equal_array_contents [6,5],         result_ids('^"cra"')
+    assert_equal_array_contents [6,5,4],       result_ids('^"cr"')
+    assert_equal_array_contents [6,5,4,3,2,1], result_ids('^"c"')
   end
 
   def test_find_options
@@ -157,8 +128,8 @@ class ActsAsIndexedTest < ActiveSupport::TestCase
   def test_update_record_bug
     p = Post.find(6)
     assert p.update_attributes(:body => p.body + ' crane')
-    assert_equal 2, Post.find_with_index('crane',{},{:ids_only => true}).size
-    assert_equal 2, Post.find_with_index('ship',{},{:ids_only => true}).size
+    assert_equal 2, result_ids('crane').size
+    assert_equal 2, result_ids('ship').size
   end
 
   # If an if proc is supplied, the index should only be created if the proc evaluated true
@@ -166,7 +137,7 @@ class ActsAsIndexedTest < ActiveSupport::TestCase
     Post.acts_as_indexed :fields => [:title, :body], :if => Proc.new { |post| post.visible }
 
     original_post_count = Post.count
-    assert_equal [], Post.find_with_index('badger')
+    assert_equal [], Post.find_with_index('badger', {}, { :no_query_cache => true, :ids_only => true})
     p = Post.new(:title => 'badger', :body => 'thousands of them!', :visible => true)
     assert p.save
     assert_equal original_post_count + 1, Post.count
@@ -222,6 +193,16 @@ class ActsAsIndexedTest < ActiveSupport::TestCase
     p = Post.find(5)
     assert p.update_attributes(:visible => false)
     assert_equal 1, Post.find_with_index('crane',{},{ :no_query_cache => true, :ids_only => true}).size
+  end
+
+  private
+  
+  def result_ids(query)
+    Post.with_query(query).map { |r| r.id }
+  end
+
+  def assert_equal_array_contents(a, b)
+    a.sort == b.sort
   end
 
 end
