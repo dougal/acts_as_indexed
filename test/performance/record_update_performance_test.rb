87ad4e881fc48730b3b128982922b133bcd39346
Douglas F Shearer <dougal.s@gmail.com>
2013-01-09 14:48:38 +0000

    Made the update performance test file more general. Added a performance test for regular record update.

---
+++ test/performance/record_update_performance_test.rb
@@ -0,0 +1,32 @@
+require 'integration_test_helper.rb'
+require 'benchmark'
+
+class RecordUpdatePerformance < ActiveSupport::TestCase
+  fixtures :posts
+
+  def teardown
+    # need to do this to work with the :if Proc tests.
+    Post.acts_as_indexed :fields => [:title, :body]
+    destroy_index
+  end
+
+  def test_unchanged
+    iterations = 10_000
+    puts "Unchanged record update (x #{ iterations })"
+
+    puts Benchmark.measure { iterations.times { posts(:wikipedia_article_1).save } }
+  end
+
+  def test_changed
+    iterations = 200
+    puts "Changed record update (x #{ iterations })"
+
+    original_title = posts(:wikipedia_article_1).title
+
+    puts Benchmark.measure {
+      iterations.times do |index|
+        posts(:wikipedia_article_1).update_attribute(:title, "#{original_title} #{ index }")
+      end
+    }
+  end
+end
